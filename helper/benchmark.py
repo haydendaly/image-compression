3480f7f17982cd2e524ce96e19dce1e0d902e5cf
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-11-28 18:45:05 -0500

    chore: use 0 to denote a black image
    I was worried because our compression ratio was actually LESS THAN
    1 for the previous string so I got worried. These results shows
    that there is an actual reduction of bytes when we use only 0.
    
    But, we still have to use the actual images.
    
    For reference in how another person parses PPM files, check this:
    
    https://github.com/joeatwork/python-lzw/blob/master/tests/tests.py

--- helper/benchmark.py
+++ helper/benchmark.py
@@ -4,7 +4,7 @@ from helper.constants import cols
 
 
 def benchmark(algo, results):
-    raw_bytes = bytes("I am the bytes of an image file", "utf-8")
+    raw_bytes = bytes("0" * 10000, "utf-8")
 
     # time compression
     compress_start = perf_counter_ns()
@@ -28,12 +28,15 @@ def benchmark(algo, results):
     bits_per_pixel = 10
 
     # record results
-    result = Series([
-        algo.__name__.split(".")[1],
-        compress_end - compress_start,
-        decompress_end - decompress_start,
-        compression_ratio,
-        source_entropy,
-        bits_per_pixel
-    ], index=cols)
+    result = Series(
+        [
+            algo.__name__.split(".")[1],
+            compress_end - compress_start,
+            decompress_end - decompress_start,
+            compression_ratio,
+            source_entropy,
+            bits_per_pixel,
+        ],
+        index=cols,
+    )
     return results.append(result, ignore_index=True)
