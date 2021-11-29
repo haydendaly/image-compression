1622e3b2283d4d21eeedb27a6214874647b6efd5
nehamanjunath3 <nm468@cornell.edu>
2021-11-29 03:07:46 -0500

    arithmetic

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 from pandas import DataFrame
-<<<<<<< HEAD
-from algorithms import rle, zlib
-from algorithms.arithmetic import arithmetic
-=======
 from algorithms import rle, zlib, lzw
->>>>>>> 62994d974b59166ef87c2d8c17cbef13995c476f
 from helper.benchmark import benchmark
 from helper.constants import cols
 
 algos = [
-<<<<<<< HEAD
-    # rle, 
-    zlib, arithmetic
-=======
     # rle,
     zlib,
     lzw,
->>>>>>> 62994d974b59166ef87c2d8c17cbef13995c476f
 ]
 
 
