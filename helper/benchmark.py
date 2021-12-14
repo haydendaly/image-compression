cc76a3632582b1960519cd97cb0bda5d9aa9d7cb
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-12-13 19:12:48 -0500

    feat: add find_benchmark_images function

--- helper/benchmark.py
+++ helper/benchmark.py
@@ -1,15 +1,25 @@
-from pandas import Series
+from pathlib import Path
+from pandas import Series, DataFrame
 from time import perf_counter_ns
 from helper.constants import cols
 from PIL import Image
 import io
 
 
-def benchmark(algo, results): 
+def find_benchmark_images():
+    """Return the images in the images sub-folders or rgb + gray"""
+    images = Path("images/rgb8bit").glob("*.ppm")
+    images_2 = Path("images/gray8bit").glob("*.pgm")
+    joined_paths = list(images) + list(images_2)
+    return [*map(str, joined_paths)]
+
+
+def benchmark(algo, results: DataFrame):
+    # TODO: Benchmark more than one image
     image = Image.open("images/rgb8bit/artificial.ppm")
-    imgByteArr = io.BytesIO()
-    image.save(imgByteArr, format=image.format)
-    raw_bytes = imgByteArr.getvalue()
+    img_byte_array = io.BytesIO()
+    image.save(img_byte_array, format=image.format)
+    raw_bytes = img_byte_array.getvalue()
 
     # time compression
     compress_start = perf_counter_ns()
@@ -28,16 +38,10 @@ def benchmark(algo, results):
     decompress_end = perf_counter_ns()
 
     # compression ratio
+    # TODO: Fix compression ratio to use bytes instead of len of the byte data
+    # structure
     compressed_size = len(compressed_bytes)
     compression_ratio = len(raw_bytes) / (compressed_size if compressed_size else 1)
-
-    # source entropy
-    # TODO
-    source_entropy = 10
-
-    # bits per pixel
-    # TODO
-    bits_per_pixel = 10
 
     # record results
     result = Series(
@@ -46,8 +50,6 @@ def benchmark(algo, results):
             compress_end - compress_start,
             decompress_end - decompress_start,
             compression_ratio,
-            source_entropy,
-            bits_per_pixel,
         ],
         index=cols,
     )
