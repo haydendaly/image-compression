d5934e8a6816662cbdbc71d207eab98e871f986b
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-12-13 19:23:55 -0500

    feat: main file benchmarks over all the images

--- helper/constants.py
+++ helper/constants.py
@@ -1,8 +1,13 @@
-cols = ["algorithm", "compress_time", "decompress_time", "compression_ratio", "source_entropy", "bits_per_pixel"]
+cols = [
+    "algorithm",
+    "image",
+    "compress_time",
+    "decompress_time",
+    "compression_ratio",
+]
+
 units = {
     "compress_time": "nanoseconds",
     "decompress_time": "nanoseconds",
     "compression_ratio": "raw / compressed bytes",
-    "source_entropy": "TODO", 
-    "bits_per_pixel": "TODO"
 }
